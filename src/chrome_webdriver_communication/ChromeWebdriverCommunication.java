94ddb8f4dbde97d6bad72e4d5510244edb860d3a
Raju <raju.pillai@a-cti.com>
2016-11-23 10:13:21 +0530

    Update ChromeWebdriverCommunication.java

--- src/chrome_webdriver_communication/ChromeWebdriverCommunication.java
+++ src/chrome_webdriver_communication/ChromeWebdriverCommunication.java
@@ -17,9 +17,9 @@ public class ChromeWebdriverCommunication {
 		
 		// This will start the chrome 
 		ChromeDriverService service = new ChromeDriverService.Builder()
-	     .usingDriverExecutable(new File("chromedriver/chromedriver"))
-	     .usingAnyFreePort()
-	     .build();
+	        .usingDriverExecutable(new File("chromedriver/chromedriver"))
+	        .usingAnyFreePort()
+	        .build();
 		service.start();
 		 
 		// Setting DesiredCapabilities for chrome
@@ -30,31 +30,30 @@ public class ChromeWebdriverCommunication {
 		// --remote-debugging-port Chromium 
 		String remoteDebuggingAddress = "localhost:9222";
 		options.setExperimentalOption("debuggerAddress", remoteDebuggingAddress);
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+                capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         
-        // Create a WebDriver instance using URL provided by the ChromeDriverService and capabilities with
-        WebDriver driver = new RemoteWebDriver(service.getUrl(), capabilities);
-        java.util.Set<java.lang.String> windowHandles = driver.getWindowHandles();
-        System.out.println(windowHandles);
+               // Create a WebDriver instance using URL provided by the ChromeDriverService and capabilities with
+                WebDriver driver = new RemoteWebDriver(service.getUrl(), capabilities);
+                java.util.Set<java.lang.String> windowHandles = driver.getWindowHandles();
+                System.out.println(windowHandles);
         
-        //pass your window ID if you have multiple window 
-        driver.switchTo().window("CDwindow-E1C85686-476B-47A1-8150-61A62975EC59");	
+               //pass your window ID if you have multiple window 
+               driver.switchTo().window("CDwindow-E1C85686-476B-47A1-8150-61A62975EC59");	
 		
 	}
 	
 	  public void StartYourApp(){  
 		   
-		  try{
-			  ChromeDriverService service = new ChromeDriverService.Builder()
-		      . usingDriverExecutable(new File("/Applications/AnywhereWorks.app/Contents/MacOS/AnywhereWorks")) 
-		      .usingAnyFreePort()
-		      .build();
-			  
-		       service.start();
+		try{
+	             ChromeDriverService service = new ChromeDriverService.Builder()
+		     .usingDriverExecutable(new File("/Applications/AnywhereWorks.app/Contents/MacOS/AnywhereWorks")) 
+		     .usingAnyFreePort()
+		     .build();
+		      service.start();
 			  }
 		  
 		  catch(Exception ex){
-			   }
+			  }
 	  }
 	  
 	  public static void main(String args[]) throws IOException{
